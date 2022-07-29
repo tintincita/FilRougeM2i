4097884c284a9d9ef3148631eddfd3a9826bdd15
Coraliedev <coralie.acquaert@gmail.com>
2022-07-29 20:51:41 +0200

    feat(auth): user password encryption

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -39,6 +40,12 @@ const userSchema = new mongoose.Schema({
     minLength: 6,
   },
 });
+
+userSchema.pre("save", async function(next){
+  const salt = await bcrypt.genSalt();
+  this.password= await bcrypt.hash(this.password, salt);
+  next();
+})
 
 const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
