da1a7c611a11c1a15abeab69e93b04942c5c8482
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-07-30 16:27:57 +0200

    docs: Function documentation in user.controller.js added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,10 @@
-const userRouter= require('express').Router();
-const userController= require('../controllers/user.controller');
+const userRouter = require("express").Router();
+const userController = require("../controllers/user.controller");
 
-userRouter.post("/create",userController.createUser);
-
-userRouter.get('/',userController.getAllUsers);
-userRouter.get('/:id',userController.getUserByID);
-userRouter.put('/:id', userController.updateUserByID);
-userRouter.delete('/:id', userController.deleteUserByID);
-
+userRouter.post("/create", userController.createUser);
+userRouter.get("/", userController.getAllUsers);
+userRouter.get("/:id", userController.getUserByID);
+userRouter.put("/:id", userController.updateUserByID);
+userRouter.delete("/:id", userController.deleteUserByID);
 
 module.exports = userRouter;
