4c5165313fd7d0dc723553cb1e08d2a21afd42e1
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-10-05 21:50:23 +0200

    wip: Reworked logic of the document save to handle situation where there is only one effect to run. Improved document readability with new lines.

--- src/controllers/card/create-card/create-card.effects.js
+++ src/controllers/card/create-card/create-card.effects.js
@@ -1,32 +1,37 @@
 const Document = require("../../../models/document.model");
 
 // It seems that a document version (document.__v) can only be updated once per request
-// hence all the mechanics around the document save ( comment : "//Save document" )
+// hence all the mechanics around the document save ( check commented section "//Save document" )
 
 module.exports.createCardEffect = {
   Document: {
-    effects: 2,
+    effects: 1,
     effectsCounter: 0,
 
     outlinerCards: async (entity, entityID, documentID) => {
       if (entity) {
         documentID = documentID.toString();
         const document = await Document.findById(documentID);
+
         if (document) {
           document.outlinerCards = document.outlinerCards.concat(entityID);
 
           // Save document
           const effects = this.createCardEffect.Document.effects;
           this.createCardEffect.Document.effectsCounter++;
+          const __v = document.__v;
+
           if (this.createCardEffect.Document.effectsCounter < effects) {
-            const __v = document.__v;
             document.__v = __v;
+
             await document.save();
           }
+
           if (this.createCardEffect.Document.effectsCounter === effects) {
-            const __v = document.__v + 1;
             document.__v = __v;
+
             await document.save();
+
             this.createCardEffect.Document.effectsCounter = 0;
           }
         }
@@ -37,21 +42,26 @@ module.exports.createCardEffect = {
       if (entity) {
         documentID = documentID.toString();
         const document = await Document.findById(documentID);
+
         if (document) {
           document.editorCards = document.editorCards.concat(entityID);
 
           // Save document
           const effects = this.createCardEffect.Document.effects;
           this.createCardEffect.Document.effectsCounter++;
+          const __v = document.__v;
+
           if (this.createCardEffect.Document.effectsCounter < effects) {
-            const __v = document.__v;
             document.__v = __v;
+
             await document.save();
           }
+
           if (this.createCardEffect.Document.effectsCounter === effects) {
-            const __v = document.__v + 1;
             document.__v = __v;
+
             await document.save();
+
             this.createCardEffect.Document.effectsCounter = 0;
           }
         }
