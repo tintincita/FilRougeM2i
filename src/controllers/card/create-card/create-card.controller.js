6908d35753dd602968468125b02865fadfdd19c9
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-10-06 08:48:44 +0200

    fix: New method to define and use controller effects.

--- src/controllers/card/create-card/create-card.controller.js
+++ src/controllers/card/create-card/create-card.controller.js
@@ -1,17 +1,12 @@
 const Card = require("../../../models/card.model");
+const { Entity } = require("../../../structures/entities.structure");
 const { getBody } = require("../../../structures/get-body.structure");
 const { createEntity } = require("../../.entity/create-entity.controller");
-const { createCardEffect } = require("./create-card.effects");
+const { createCardEffects } = require("./create-card.effects");
 
 module.exports.createCard = (request, response) => {
   const card = new Card(getBody(Card.modelName, request));
 
-  // The number of runned effects must be set manually in create-card.effects.js.
-  // This surely can be improved.
-  if (createCardEffect.Document.effects === 2) {
-    createCardEffect.Document.outlinerCards(card, card.id, card.document);
-    createCardEffect.Document.editorCards(card, card.id, card.document);
-  }
-
   createEntity(Card, card, response);
+  createCardEffects(Entity.Document, card.document, card.id);
 };
--- src/controllers/card/create-card/create-card.effects.js
+++ src/controllers/card/create-card/create-card.effects.js
@@ -1,71 +1,92 @@
+const { Entity } = require("../../../structures/entities.structure");
 const Document = require("../../../models/document.model");
+const terminal = require("../../../middlewares/terminal.middlewares");
+
+module.exports.createCardEffects = async (entity, documentID, cardID) => {
+  if (entity === Entity.Document) {
+    try {
+      documentID = documentID.toString();
+      const document = await Document.findById(documentID);
+
+      if (document) {
+        document.outlinerCards = document.outlinerCards.concat(cardID);
+        document.editorCards = document.editorCards.concat(cardID);
+        document.save();
+      } else {
+        terminal.log("Document not found");
+      }
+    } catch (error) {
+      terminal.log(error);
+    }
+  }
+};
 
 // It seems that a document version (document.__v) can only be updated once per request
 // hence all the mechanics around the document save.
 // Check commented section starting by "// Save document - Begin" and finishing by "// Save document - End".
 
-module.exports.createCardEffect = {
-  Document: {
-    effects: 2,
-    effectsCounter: 0,
+// module.exports.createCardEffect = {
+//   Document: {
+//     effects: 1,
+//     effectsCounter: 0,
 
-    outlinerCards: async (entity, entityID, documentID) => {
-      if (entity) {
-        documentID = documentID.toString();
-        const document = await Document.findById(documentID);
+//     outlinerCards: async (entity, entityID, documentID) => {
+//       if (entity) {
+//         documentID = documentID.toString();
+//         const document = await Document.findById(documentID);
 
-        if (document) {
-          document.outlinerCards = document.outlinerCards.concat(entityID);
+//         if (document) {
+//           document.outlinerCards = document.outlinerCards.concat(entityID);
 
-          // Save document - Begin
-          const effects = this.createCardEffect.Document.effects;
-          this.createCardEffect.Document.effectsCounter++;
-          const __v = document.__v;
+//           // Save document - Begin
+//           const effects = this.createCardEffect.Document.effects;
+//           this.createCardEffect.Document.effectsCounter++;
+//           const __v = document.__v;
 
-          if (this.createCardEffect.Document.effectsCounter < effects) {
-            document.__v = __v;
-            await document.save();
-          }
+//           if (this.createCardEffect.Document.effectsCounter < effects) {
+//             document.__v = __v;
+//             await document.save();
+//           }
 
-          if (this.createCardEffect.Document.effectsCounter === effects) {
-            document.__v = __v + 1;
-            await document.save();
-            this.createCardEffect.Document.effectsCounter = 0;
-          }
-          // Save document - End
-        }
-      }
-    },
+//           if (this.createCardEffect.Document.effectsCounter === effects) {
+//             document.__v = __v + 1;
+//             await document.save();
+//             this.createCardEffect.Document.effectsCounter = 0;
+//           }
+//           // Save document - End
+//         }
+//       }
+//     },
 
-    editorCards: async (entity, entityID, documentID) => {
-      if (entity) {
-        documentID = documentID.toString();
-        const document = await Document.findById(documentID);
+//     editorCards: async (entity, entityID, documentID) => {
+//       if (entity) {
+//         documentID = documentID.toString();
+//         const document = await Document.findById(documentID);
 
-        if (document) {
-          document.editorCards = document.editorCards.concat(entityID);
+//         if (document) {
+//           document.editorCards = document.editorCards.concat(entityID);
 
-          // Save document - Begin
-          const effects = this.createCardEffect.Document.effects;
-          this.createCardEffect.Document.effectsCounter++;
-          const __v = document.__v;
+//           // Save document - Begin
+//           const effects = this.createCardEffect.Document.effects;
+//           this.createCardEffect.Document.effectsCounter++;
+//           const __v = document.__v;
 
-          if (this.createCardEffect.Document.effectsCounter < effects) {
-            document.__v = __v;
-            await document.save();
-          }
+//           if (this.createCardEffect.Document.effectsCounter < effects) {
+//             document.__v = __v;
+//             await document.save();
+//           }
 
-          if (this.createCardEffect.Document.effectsCounter === effects) {
-            document.__v = __v + 1;
-            await document.save();
-            this.createCardEffect.Document.effectsCounter = 0;
-          }
-          // Save document - End
-        }
-      }
-    },
-    //
-    // Next effects...
-    //
-  },
-};
+//           if (this.createCardEffect.Document.effectsCounter === effects) {
+//             document.__v = __v + 1;
+//             await document.save();
+//             this.createCardEffect.Document.effectsCounter = 0;
+//           }
+//           // Save document - End
+//         }
+//       }
+//     },
+//     //
+//     // Next effects...
+//     //
+//   },
+// };
