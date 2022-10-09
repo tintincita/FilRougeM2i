731b8a85409c98610eedf8797bb32cfd94b66fef
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-10-09 18:23:49 +0200

    feat: Creating a project adds it to its workspace.

--- src/controllers/project/create-project/create-project.controller.js
+++ src/controllers/project/create-project/create-project.controller.js
@@ -1,9 +1,11 @@
 const Project = require("../../../models/project.model");
 const { getBody } = require("../../../structures/get-body.structure");
 const { createEntity } = require("../../.entity/create-entity.controller");
+const { createProjectEffects } = require("./effects/.create-project.effects");
 
 module.exports.createProject = (request, response) => {
   const project = new Project(getBody(Project.modelName, request));
 
   createEntity(Project, project, response);
+  createProjectEffects(project);
 };
---
+++ src/controllers/project/create-project/effects/.create-project.effects.js
@@ -0,0 +1,32 @@
+const Project = require("../../../../models/project.model");
+const Workspace = require("../../../../models/workspace.model");
+const { workspaceProjects } = require("./workspace.projects.effects");
+
+// The use of a terminal log is required here.
+// Only one response can be send to the client,
+// in our case the final response is send from the entity controller.
+const terminal = require("../../../../middlewares/terminal.middlewares");
+const { message } = require("../../../../structures/messages.structure");
+
+module.exports.createProjectEffects = async (entity) => {
+  try {
+    if (entity) {
+      const workspaceID = entity.workspace.toString();
+      const workspace = await Workspace.findById(workspaceID);
+
+      if (workspace) {
+        const projectID = entity._id.toString();
+
+        workspaceProjects(workspace, projectID);
+
+        // Document save can only be done once per request
+        workspace.save();
+        //
+      } else {
+        terminal.log(message.error.readEntity(Project.modelName, projectID));
+      }
+    }
+  } catch (error) {
+    terminal.log(error);
+  }
+};
