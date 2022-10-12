b897d9b821877afa95add6a2646151b93aef52d1
merlincita <merlincita@gmail.com>
2022-10-12 10:15:34 +0200

    Update: card_api to match new codes

--- tests/card_api.test.js
+++ tests/card_api.test.js
@@ -33,18 +33,14 @@ test('cards are returned as json', async () => {
 test('malformatted id returns error', async () => {
   await api
     .get('/api/card/88')
-    // current model throws 500. change try catch on .entity controller to get 404, etc
-    // .expect(400)
-    .expect(500)
+    .expect(404)
 })
 
 test('unexisting id returns error', async () => {
   let id = helper.nonExistingCardId();
   await api
     .get(`/api/card/${id}`)
-    // current model throws 500. change try catch on .entity controller to get 404, etc
-    // .expect(400)
-    .expect(500)
+    .expect(404)
 })
 
 
@@ -72,11 +68,9 @@ describe('DELETE card', () => {
   test('a card can be deleted', async () => {
     const cardsAtStart = await helper.cardsInDb()
     const cardToDelete = cardsAtStart[0]
-    console.log(cardToDelete);
-    console.log(cardToDelete._id);
     await api
       .delete(`/api/card/${cardToDelete._id}`)
-      .expect(200)
+      .expect(204)
 
     const cardsAtEnd = await helper.cardsInDb()
 
@@ -90,22 +84,20 @@ describe('DELETE card', () => {
 
   test('parent document is updated', async () => {
     const cardsAtStart = await helper.cardsInDb()
-    const cardToDelete = cardsAtStart[0]
+    const cardToUpdate = cardsAtStart[0]
 
     await api
-      .delete(`/api/card/${cardToDelete._id}`)
-      // .expect(204)
-      .expect(200)
+      .delete(`/api/card/${cardToUpdate._id}`)
 
-    const parentDoc = await Document.findById(cardToDelete.document)
+    const parentDoc = await Document.findById(cardToUpdate.document)
 
-    expect(parentDoc.outlinerCards).not.toContain(cardToDelete._id)
-    expect(parentDoc.editorCards).not.toContain(cardToDelete._id)
+    expect(parentDoc.outlinerCards).not.toContain(cardToUpdate._id)
+    expect(parentDoc.editorCards).not.toContain(cardToUpdate._id)
   })
 
 })
 
-test.only('a card can be updated', async () => {
+test('a card can be updated', async () => {
   const cardsAtStart = await helper.cardsInDb()
 
   const cardToChange = cardsAtStart[0]
@@ -117,7 +109,7 @@ test.only('a card can be updated', async () => {
   await api
     .put(`/api/card/${cardToChange._id}`)
     .send(changesToCard)
-    .expect(200)
+    .expect(202)
     .expect('Content-Type', /application\/json/)
 
   const response = await api.get('/api/card')
